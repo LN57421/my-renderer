dcbf4369d1fa03890d9203116c99846c54bf9b59
Liu Nian <12110416@mail.sustech.edu.cn>
2024-10-05 21:26:18 +0800

    triangles drawing method2&3, back-face culling implements

--- test/draw_triangles.cpp
+++ test/draw_triangles.cpp
@@ -31,10 +31,78 @@ void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
     }
 }
 
+// mothod1: draw the triangle by drawing the three edges
+// void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+//     line(t0, t1, image, color);
+//     line(t1, t2, image, color);
+//     line(t2, t0, image, color);
+// }
+
+// mothod2: more efficient, plus fill the triangle
+// void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+//     if (t0.y == t1.y && t0.y == t2.y)
+//         return;
+//     // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!) 
+//     if (t0.y>t1.y) std::swap(t0, t1); 
+//     if (t0.y>t2.y) std::swap(t0, t2); 
+//     if (t1.y>t2.y) std::swap(t1, t2); 
+//     int total_height = t2.y-t0.y; 
+//     for (int y=t0.y; y<=t1.y; y++) { 
+//         int segment_height = t1.y-t0.y+1; 
+//         float alpha = (float)(y-t0.y)/total_height; 
+//         float beta  = (float)(y-t0.y)/segment_height; // be careful with divisions by zero 
+//         Vec2i A = t0 + (t2-t0)*alpha; 
+//         Vec2i B = t0 + (t1-t0)*beta; 
+//         if (A.x>B.x) std::swap(A, B); 
+//         for (int j=A.x; j<=B.x; j++) { 
+//             image.set(j, y, color); // attention, due to int casts t0.y+i != A.y 
+//         } 
+//     } 
+//     for (int y=t1.y; y<=t2.y; y++) { 
+//         int segment_height =  t2.y-t1.y+1; 
+//         float alpha = (float)(y-t0.y)/total_height; 
+//         float beta  = (float)(y-t1.y)/segment_height; // be careful with divisions by zero 
+//         Vec2i A = t0 + (t2-t0)*alpha; 
+//         Vec2i B = t1 + (t2-t1)*beta; 
+//         if (A.x>B.x) std::swap(A, B); 
+//         for (int j=A.x; j<=B.x; j++) { 
+//             image.set(j, y, color); // attention, due to int casts t0.y+i != A.y 
+//         } 
+//     } 
+// }
+
+// mothod3: refactoring mothod2
 void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
-    line(t0, t1, image, color);
-    line(t1, t2, image, color);
-    line(t2, t0, image, color);
+    if (t0.y == t1.y && t0.y == t2.y)
+        return;  // I dont care about degenerate triangles
+    // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!)
+    if (t0.y > t1.y)
+        std::swap(t0, t1);
+    if (t0.y > t2.y)
+        std::swap(t0, t2);
+    if (t1.y > t2.y)
+        std::swap(t1, t2);
+    int total_height = t2.y - t0.y;
+    for (int i = 0; i < total_height; i++) {
+        bool second_half = i > t1.y - t0.y || t1.y == t0.y;
+        int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+
+        // linear interpolation factor
+        float alpha = (float)i / total_height;
+        float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) /
+                     segment_height;  // be careful: with above conditions no
+                                      // division by zero here
+        //
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+        
+        if (A.x > B.x)
+            std::swap(A, B);
+        for (int j = A.x; j <= B.x; j++) {
+            image.set(j, t0.y + i,
+                      color);  // attention, due to int casts t0.y+i != A.y
+        }
+    }
 }
 
 int main(int argc, char** argv) {

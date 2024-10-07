86b6cc5b6c41fbeeb3415086c36a31e89ee28cb1
Liu Nian <12110416@mail.sustech.edu.cn>
2024-10-07 18:00:52 +0800

    z-buffer&barycentric

--- include/model.h
+++ include/model.h
@@ -2,19 +2,21 @@
 #define __MODEL_H__
 
 #include <vector>
+
 #include "geometry.h"
 
 class Model {
 private:
-	std::vector<Vec3f> verts_;
-	std::vector<std::vector<int> > faces_;
+    std::vector<Vec3f> verts_;
+    std::vector<std::vector<int> > faces_;
+
 public:
-	Model(const char *filename);
-	~Model();
-	int nverts();
-	int nfaces();
-	Vec3f vert(int i);
-	std::vector<int> face(int idx);
+    Model(const char *filename);
+    ~Model();
+    int nverts();
+    int nfaces();
+    Vec3f vert(int i);
+    std::vector<int> face(int idx);
 };
 
-#endif //__MODEL_H__
+#endif  //__MODEL_H__
